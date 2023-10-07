8ed4253b200ec31caefb47c01e07950935f650ed
MdShamimIslam <shamim401897@gmail.com>
2023-10-07 10:27:14 +0600

    useAxiosSecure used

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -2,7 +2,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { useContext } from "react";
 import { authContext } from "../Providers/AuthProvider";
-// import useAxiosSecure from "./useAxiosSecure";
+
 
 
 const useCart = () => {
